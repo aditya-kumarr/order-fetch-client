0f41c5a18d915106721a0c2f0a3cf10819d08981
aditya <adityakumar222333@gmail.com>
2022-09-29 20:12:40 +0530

    💥 NEW: added styles for default button

--- src/GlobalStyles.style.js
+++ src/GlobalStyles.style.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
- const GlobalStyles = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
 *,*::after,*::before{
     margin:0;
     padding: 0;
@@ -11,6 +11,13 @@ h1,h2,h3,h4,h5,h6,div,p{
     font-family: sans-serif;
     font-size: 1rem;
     font-weight: 500;
+}
+
+button{
+    border: none;
+    background-color: transparent;
+    box-shadow: none;
+    text-shadow: none;
 }
 
 `;
