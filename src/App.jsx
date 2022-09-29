340d575aac8b28ad5f219df35cde77147d43c1b5
aditya <adityakumar222333@gmail.com>
2022-09-29 19:54:16 +0530

    🎉 Initial Commit: Removed all the sample code from vite Changed the Title in index.html Removed the strict mode

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles.style";
+import { CardView } from "./styles";
+
+function App() {
+  // A serch bar for seraching for users
+  // A serch bar for seraching for orders
+  // A container for lists of userData or order Data
+  // when clicked on an a result show more information on a different page.
+  return (
+    <div>
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
